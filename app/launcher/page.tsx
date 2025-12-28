66b97dbb380339589feaf5a7fe77a68d76f98519
RedFly <maxences427@gmail.com>
2025-12-28 10:50:15 +0100

    update links

--- app/launcher/page.tsx
+++ app/launcher/page.tsx
@@ -48,14 +48,14 @@ const DOWNLOADS = [
 
 const DOWNLOAD_LINKS = {
   WINDOWS: {
-    X64: "https://github.com/divizion-project/Divizion-Launcher/releases/download/3.0.1/Divizion-Launcher-setup-3.0.1.exe"
+    X64: "https://github.com/divizion-project/Divizion-Launcher/releases/download/3.0.2/Divizion-Launcher-setup-3.0.2.exe"
   },
   MACOS: {
-    X64: "https://github.com/divizion-project/Divizion-Launcher/releases/download/3.0.1/Divizion-Launcher-setup-3.0.1-x64.dmg",
-    ARM: "https://github.com/divizion-project/Divizion-Launcher/releases/download/3.0.1/Divizion-Launcher-setup-3.0.1-arm64.dmg"
+    X64: "https://github.com/divizion-project/Divizion-Launcher/releases/download/3.0.2/Divizion-Launcher-setup-3.0.2-x64.dmg",
+    ARM: "https://github.com/divizion-project/Divizion-Launcher/releases/download/3.0.2/Divizion-Launcher-setup-3.0.2-arm64.dmg"
   },
   LINUX: {
-    APPIMAGE: "https://github.com/divizion-project/Divizion-Launcher/releases/download/3.0.1/Divizion-Launcher-3.0.1.AppImage"
+    APPIMAGE: "https://github.com/divizion-project/Divizion-Launcher/releases/download/3.0.2/Divizion-Launcher-3.0.2.AppImage"
   }
 };
 
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 export const DISCORD_LINK = "https://discord.gg/CrfM2WVKYh";
 export const GITHUB_LINK = "https://github.com/divizion-project";
-export const LAUNCHER_VERSION = "3.0.1";
+export const LAUNCHER_VERSION = "3.0.2";
 export const LAUNCHER_RELEASES_LINK =
 	"https://github.com/divizion-project/Divizion-Launcher/releases";
